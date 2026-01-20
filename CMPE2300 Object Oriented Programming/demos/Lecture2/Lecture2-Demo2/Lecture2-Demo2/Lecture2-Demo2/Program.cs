df4ea3d99e323e71f38bf26c29e1d2151ebd7db7
ommpatel142 <ommpatel142@gmail.com>
2026-01-20 07:07:32 -0700

    Lecture 2

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture2/Lecture2-Demo2/Lecture2-Demo2/Lecture2-Demo2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+CMPE 2300 Object-Oriented Programming
+
+Lecture2 - Demo2 - Working with Classes
+
+Date: Jan 15th, 2026
+
+Author: Luke Picherack
+ */
+
+using Lecture2_Demo2;
+
+//Creatine an instance (an object) of the CStudent class
+CStudent s1 = new CStudent("Jackson", 97);
+CStudent s2 = new CStudent("Micael");
+CStudent s3 = new CStudent("Harry, 95, 525");
+Console.WriteLine($"The student s1 has the following attributes: {s1.ToString()}");
+Console.WriteLine($"The student s2 has the following attributes: {s2.ToString()}");
+Console.WriteLine($"The student s3 has the following attributes: {s3.ToString()}");
+
+
+//Press any key to exit
+Console.WriteLine("Press Any Key to Exit");
+Console.ReadKey();
