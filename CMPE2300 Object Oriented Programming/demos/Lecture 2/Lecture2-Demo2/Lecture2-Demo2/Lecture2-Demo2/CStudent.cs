2db2f418e9cb43c9ccb7622bb6f6c97ff045b2ea
ommpatel142 <ommpatel142@gmail.com>
2026-01-20 10:12:50 -0700

    lecture 2

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture 2/Lecture2-Demo2/Lecture2-Demo2/Lecture2-Demo2/CStudent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture2_Demo2
+{
+    internal class CStudent
+    {
+        private string _name;
+        private double _grade;
+        private decimal _fees_due;
+
+        //User-defined constructor
+        public CStudent(string name, double grade)
+        {
+            _name = name;
+            _grade = grade;
+        }
+
+        public CStudent(string name, double grade, decimal fees):this(name, grade)
+        {
+            _fees_due = fees;
+        }
+
+        //user-defined constructor with only the name as parameter
+        public CStudent(string name)
+        {
+            _name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {_name}, grade: {_grade}, fees: {_fees_due}";
+
+        }
+    }
+}
