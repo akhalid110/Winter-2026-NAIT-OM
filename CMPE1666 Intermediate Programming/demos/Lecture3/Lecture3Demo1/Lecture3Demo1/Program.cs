e0bcdf15330cda6ca4806d7806e826f83eadff3b
ommpatel142 <ommpatel142@gmail.com>
2026-01-29 17:19:04 -0700

    demo

---
+++ CMPE1666 Intermediate Programming/demos/Lecture3/Lecture3Demo1/Lecture3Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+List<string> myData = new List<string>();
+myData.Add("Cat");
+myData.Add("Dog");
+myData.Add("Lion");
+myData.Insert(1, "Goat");
+Console.WriteLine(myData[0]);
+Console.WriteLine(myData[1]);
+Console.WriteLine(myData[2]);
+Console.WriteLine(myData[3]);
+//Console.WriteLine(myData[30]); // Index out of range 
+
+Console.WriteLine();
+
+Console.WriteLine($"There are {myData.Count} elements in the List ");
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("Using foreach loop");
+foreach(string s in myData)
+{
+    Console.WriteLine(s);
+}
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("Using for loop");
+for (int i = 0; i<myData.Count; i++)
+{
+    Console.WriteLine($"Animal {i} = {myData[i]}");
+}
