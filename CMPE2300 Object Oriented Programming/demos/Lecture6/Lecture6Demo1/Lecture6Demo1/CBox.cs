6dcb07b76fbbfd85b5e5d06268d3e868d9893574
ommpatel142 <ommpatel142@gmail.com>
2026-02-05 14:32:25 -0700

    demo

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture6/Lecture6Demo1/Lecture6Demo1/CBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6Demo1
+{
+    internal class CBox : IComparable
+    {
+        private int _width;
+        private int _height;
+
+        public CBox(int width , int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+
+        //override Equals
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is CBox arg)) return false;
+            return (_width == arg._width && _height == arg._height);
+        }
+
+        public override string ToString()
+        {
+            return $"Width : {_width} and Height : {_height}";
+        }
+        public int CompareTo(object obj)
+        {
+            if (!(obj is CBox arg))
+                throw new ArgumentException($"CBox: CompareTo {nameof(obj)} - is invalid");
+            return (_width * _height).CompareTo(arg._width * arg._height);
+        }
+    }
+
+}
---
+++ CMPE2300 Object Oriented Programming/demos/Lecture6/Lecture6Demo1/Lecture6Demo1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class CBox : IComparable  // Now with comparison via CompareTo()
+{
+    private int Width;
+    private int Height;
+    public CBox(int _widht, int _height)
+    {
+        this.Width = _widht;
+        this.Height = _height;
+    }
+}
