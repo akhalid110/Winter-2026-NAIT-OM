dd0c5c8545a5af6162dceff789e0b9c960cc11e3
ommpatel142 <ommpatel142@gmail.com>
2026-02-11 15:02:16 -0700

    demo

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture7/Lecture7Demo2/Lecture7Demo2/CBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7Demo2
+{
+    internal class CBox
+    {
+        private int _width;
+        private int _height;
+
+        public CBox(int Height, int Width)
+        {
+            this._width = Width;
+            this._height = Height;
+
+        }
+        public override string ToString()
+        {
+            return $"({_width},{_height})";
+        }
+
+        public static bool IsBig(CBox box)
+        {
+            return box._width * box._height > 15;
+        }
+        public int Area
+        {
+            get
+            {
+                return _width*_height;
+            }
+          
+        }
+    }
+}
---
+++ CMPE2300 Object Oriented Programming/demos/Lecture7/Lecture7Demo2/Lecture7Demo2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Lecture7Demo2;
+using System.Runtime.CompilerServices;
+CBox box1 = new CBox(4, 3);
+CBox box2 = new CBox(8, 2);
+CBox box3 = new CBox(6,3);
+CBox box4 = new CBox(2,1);
+CBox box5 = new CBox(2,3);
+CBox box6 = new CBox(9,2);
+CBox box7 = new CBox(9,3);
+CBox box8 = new CBox(8,3);
+
+
+List<CBox> list = new List<CBox> {box1, box2, box3, box4, box5, box6, box7, box8 };
+
+Console.WriteLine($"Initial list of th boxes : {string.Join(", ", list)}");
+Console.WriteLine($"list of the boxes after filtering with predicate IsBig: {string.Join(", ", list.Find(CBox.IsBig))}");
+Console.WriteLine($"list of the boxes after filtering with predicate IsBig: {string.Join(", ", list.FindAll(CBox.IsBig))}");
+Console.WriteLine($"list of the boxes after filtering with predicate IsBig: {string.Join(", ", list.FindAll(delegate(CBox box) { return box.Area > 20; }))}");
+
+
