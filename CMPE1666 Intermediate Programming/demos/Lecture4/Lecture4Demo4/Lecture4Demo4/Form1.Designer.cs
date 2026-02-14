062e8c22dec551d6d59b3f562ba9dc36bc6fed75
ommpatel142 <ommpatel142@gmail.com>
2026-02-13 20:15:39 -0700

    demo

---
+++ CMPE1666 Intermediate Programming/demos/Lecture4/Lecture4Demo4/Lecture4Demo4/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lecture4Demo4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_DisplayData_Lbx = new System.Windows.Forms.ListBox();
+            this.UI_DisplayData_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_DisplayData_Lbx
+            // 
+            this.UI_DisplayData_Lbx.FormattingEnabled = true;
+            this.UI_DisplayData_Lbx.Location = new System.Drawing.Point(13, 24);
+            this.UI_DisplayData_Lbx.Name = "UI_DisplayData_Lbx";
+            this.UI_DisplayData_Lbx.Size = new System.Drawing.Size(210, 199);
+            this.UI_DisplayData_Lbx.TabIndex = 0;
+            // 
+            // UI_DisplayData_Btn
+            // 
+            this.UI_DisplayData_Btn.Location = new System.Drawing.Point(13, 240);
+            this.UI_DisplayData_Btn.Name = "UI_DisplayData_Btn";
+            this.UI_DisplayData_Btn.Size = new System.Drawing.Size(210, 41);
+            this.UI_DisplayData_Btn.TabIndex = 1;
+            this.UI_DisplayData_Btn.Text = "Display Data ";
+            this.UI_DisplayData_Btn.UseVisualStyleBackColor = true;
+            this.UI_DisplayData_Btn.Click += new System.EventHandler(this.UI_DisplayData_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Blue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(245, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 67);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Drag And Drop Your File Here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 293);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UI_DisplayData_Btn);
+            this.Controls.Add(this.UI_DisplayData_Lbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox UI_DisplayData_Lbx;
+        private System.Windows.Forms.Button UI_DisplayData_Btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CMPE1666 Intermediate Programming/demos/Lecture4/Lecture4Demo4/Lecture4Demo4/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture4Demo4
+{
+    public partial class Form1 : Form
+    {
+        private string _fileName;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.AllowDrop = true;
+        }
+
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void label1_DragDrop(object sender, DragEventArgs e)
+        {
+            _fileName = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+            label1.Text = Path.GetFileName(_fileName);
+            MessageBox.Show($"{_fileName}");
+
+        }
+
+        private void UI_DisplayData_Btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_fileName))
+            {
+                MessageBox.Show("Please drag and drop a file first.");
+                return;
+            }
+
+            UI_DisplayData_Lbx.Items.Clear();
+            try
+            {
+                foreach (string line in File.ReadAllLines(_fileName))
+                {
+                    UI_DisplayData_Lbx.Items.Add(line);
+                }
+            }
+            catch (Exception ex)
+            {                                      
+                MessageBox.Show("Error reading file: " + ex.Message);
+            }
+        }
+    }
+}
